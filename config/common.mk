82a59ebffa8d4ffd1afdeb1c59c6076cf42d104f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:49:57 -0300

    vendor: Add permissions for lineagehw
    Change-Id: I55172ac4e9121aa969b628b450781e58151de18c

--- config/common.mk
+++ config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Lineage-specific file
+PRODUCT_COPY_FILES += \
+    vendor/blaster/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
