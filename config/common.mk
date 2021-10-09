b083097a643c2ca7964648d68e4daceb6aee9c7a
Dyneteve <dyneteve@hentaios.com>
2021-10-09 15:02:42 +0200

    vendor: overlay: Update from SP1A.210812.016
    Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -73,6 +73,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Gapps
+$(call inherit-product, vendor/aosp/config/gapps.mk)
+
 # Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
