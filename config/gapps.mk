a0dd1f8b6764aedfd3d62cce11939d10e91053db
Dyneteve <dyneteve@hentaios.com>
2021-10-09 15:02:42 +0200

    vendor: overlay: Update from SP1A.210812.016
    Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ config/gapps.mk
@@ -0,0 +1,14 @@
+# Gapps
+ifeq ($(USE_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/aosp/overlay-gapps
+
+$(call inherit-product, vendor/aosp/config/rro_overlays.mk)
+endif
