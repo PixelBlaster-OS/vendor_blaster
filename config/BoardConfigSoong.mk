93e3d0b790df1611534ef82d4b867f405db81214
LuK1337 <priv.luk@gmail.com>
2023-06-14 21:52:43 +0200

    soong: Add `aapt_version_code` default
    This appends `--version_code=$(date -u +%Y%m%d)` to aapt flags, which is
    useful for flushing some caches upon system updates.
    
    Change-Id: I6575b878f09c1c3138e12abc34d39405f51245e7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,6 +28,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += blasterGlobalVars
 SOONG_CONFIG_blasterGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     gralloc_handle_has_reserved_size \
@@ -84,6 +85,7 @@ TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
 TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 
 # Soong value variables
+SOONG_CONFIG_blasterGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_blasterGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_blasterGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_blasterGlobalVars_target_health_charging_control_charging_path := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH)
